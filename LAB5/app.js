a0cfd4aeefd778889e2b823f77a6ce1658b380dd
Devansh Chandak <51949082+dchandak99@users.noreply.github.com>
2021-03-13 17:45:05 +0530

    Update app.js

--- LAB5/app.js
+++ LAB5/app.js
@@ -8,13 +8,28 @@ const pool =  require('./utils/database');
 
 
 const app = express();
+
+const mainRo = express.Router();
+const prodCon = require('./controllers/prods')
+//const cartCon = require('./controllers/cart')
+//const orderCon = require('./controllers/order')
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 
+mainRo.get('/prods', prodCon.get_test);
+//mainRo.get('/cart', cartCon.get_test);
+//mainRo.post('/cart', cartCon.post_test);
+//mainRo.get('/orders', orderCon.get_test);
+//mainRo.post('/orders', orderCon.post_test);
+
 
 app.use('/admin',adminRo);
+app.use('/', mainRo)
 
-
-app.listen(3000);
+app.listen(3000, function(){
+    console.log("Server started on port 3000")
+})
+;
