fe915e52204bcd19cb31cb22496131e5afe5dbed
Devansh Chandak <51949082+dchandak99@users.noreply.github.com>
2021-03-13 16:22:27 +0530

    Update prods.js

--- LAB5/controllers/prods.js
+++ LAB5/controllers/prods.js
@@ -4,19 +4,15 @@ const Cart = require('../models/cart');
 
 exports.get_test = (req,res,next) => {
 
-    Prod.
-        get_all()
-        .then(() => {
-            res.redirect('/prods');
-        })
-        .catch(err => console.log(err));
-
-    res.render('prods', {
-        pageTitle: 'Products',
-        path: 'prods',
-        editing: false
+    var products = Prod.get_all();
+        products.then((result) => {
+            res.render('prods', {
+            pageTitle: 'All Products',
+            path: '/prods',
+            editing: false,
+            products: result.rows
     });
+});
 
 
 };
-
