c67ecb3c574d85120c6d986844723130a3a972cd
ParthLa <parthlaturia@gmail.com>
2021-03-14 19:53:07 +0530

    fneejo

--- LAB5/controllers/cart.js
+++ LAB5/controllers/cart.js
@@ -4,7 +4,7 @@ const Cart = require('../models/cart');
 
 exports.get_test = (req,res,next) => {
 
-    var products = Prod.get_all();
+    var products = Cart.get_all();
         products.then((result) => {
             res.render('cart', {
             pageTitle: 'All Products',
@@ -15,4 +15,18 @@ exports.get_test = (req,res,next) => {
 });
 
 
+};
+
+exports.post_test = (req,res,next) => {
+    const title = req.body.title;
+    const image = req.body.image
+    const price = req.body.price;
+    const quantity = req.body.quantity;
+    const product = new Cart( item_id,quantity);
+    product
+        .add_to_cart()
+        .then(() => {
+            res.redirect('/cart');
+        })
+        .catch(err => console.log(err));
 };
--- LAB5/models/cart.js
+++ LAB5/models/cart.js
@@ -1,18 +1,23 @@
 const pool= require('../utils/database');
-module.exports = class Prod{
+module.exports = class Cart{
 
-    constructor( title, image, price, quantity){
-        this.title = title;
-        this.image = image;
-        this.price = price;
-        this.quantity = quantity;
+    constructor( item_id, quantity){
+        // this.title = title;
+        // this.image = image;
+        // this.price = price;
+        this.user_id = 1;
+        this.item_id = item_id;
+        this.quantity = quantity; 
     }
 
-    add_prod(){
-        return pool.query('INSERT INTO products(title, price, image, quantity) VALUES ($1, $2, $3, $4);', [this.title, this.price, this.image, this.quantity]);
+    // add_prod(){
+    //     return pool.query('INSERT INTO products(user_id, item_id, quantity) VALUES ($1, $2, $3, $4);', [this.title, this.price, this.image, this.quantity]);
+    // }
+    add_to_cart(){
+        return pool.query('INSERT INTO cart(user_id, item_id, quantity) VALUES ($1, $2, $3);', [this.user_id, this.item_id, this.quantity]);
     }
     static get_all(){
-        return pool.query('SELECT * FROM products');
+        return pool.query('SELECT * FROM cart');
 
     }
 
