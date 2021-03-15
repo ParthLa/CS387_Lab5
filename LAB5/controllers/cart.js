b270385c6ac5ac0f82d7cd6cd39e80c7aec5587a
Devansh Chandak <51949082+dchandak99@users.noreply.github.com>
2021-03-15 18:43:33 +0530

    Update cart.js

--- LAB5/controllers/cart.js
+++ LAB5/controllers/cart.js
@@ -17,40 +17,39 @@ exports.get_test = (req,res,next) => {
             credits: credits.rows[0].credit
     });
 })});
-    //console.log(credits);
-
 };
 
-// exports.post_test = (req,res,next) => {
-//     const title = req.body.title;
-//     const image = req.body.image
-//     const price = req.body.price;
-//     const quantity = req.body.quantity;
-//     const user_id = req.body.user_id;
-//     const item_id = req.body.item_id;
-//     const quantity1 = req.body.quantity; // problem
-//     const cart_prod = new Cart(1, item_id, quantity);
-//     const prod = new Prod(title, image, price, quantity);
-//     // cart_prod
-//     	// call the -- function...check
-//     	if(quantity > 0){
-//     		prod.reduce_quantity(item_id)
-//     		cart_prod.add_to_cart()
-//     		cart_prod.inc_quantity()
-//     		cart_prod.then(() => {
-//             	res.redirect('/cart');
-//         	})
-//         	cart_prod.catch(err => console.log(err));
-//     	}
-//     	else{
-//     		prod.then(() => {
-//             	res.redirect('/prods');
-//         	})
-//         	prod.catch(err => console.log(err));
-//     	}
-//         // .add_to_cart()
-//         // .then(() => {
-//             // res.redirect('/cart');
-//         // })
-//         // .catch(err => console.log(err));
-// };
+exports.post_test = (req,res,next) => {
+     
+    const product_id = req.body.product_id;
+
+    const cart_prod = new Cart(1, product_id);
+
+    const info = Prod.get_info(product_id)
+    .then(() => {
+        console.log(info);
+    });
+
+    //const prod = new Prod(info.title, info.image, info.price, info.quantity);
+    //console.log(info);
+
+     	if(info.quantity > 0){
+     		Prod.reduce_quantity(product_id)
+     		//cart_prod.inc_quantity()
+            cart_prod
+            .add_to_cart()
+            .then(() => {
+             	res.redirect('/cart');
+         	})
+         	.catch(err => console.log(err));
+     	}
+     	else{
+     		//cart_prod
+    
+            //.then(() => {
+             	res.redirect('/prods');
+         	//})
+         	//.catch(err => console.log(err));
+     	}
+
+ };
