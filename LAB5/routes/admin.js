661d0b3e0cb66cd1c461e3a13c13143162af750c
Devansh Chandak <51949082+dchandak99@users.noreply.github.com>
2021-03-15 18:45:02 +0530

    Update admin.js

--- LAB5/routes/admin.js
+++ LAB5/routes/admin.js
@@ -8,9 +8,5 @@ const router = express.Router();
 
 router.get('/add-product',adminCon.get_test);
 router.post('/add-product',adminCon.post_test);
-router.get('/prods',adminCon.get_products);
-router.get('/cart',adminCon.get_test);
-router.get('/orders',adminCon.get_test);
-
 
 module.exports = router;
