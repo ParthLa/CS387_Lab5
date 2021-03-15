bb53073bbdb49621b261e6faf36b3ef152962ab6
Devansh Chandak <51949082+dchandak99@users.noreply.github.com>
2021-03-15 18:44:30 +0530

    Update cart.js

--- LAB5/models/cart.js
+++ LAB5/models/cart.js
@@ -2,25 +2,23 @@ const pool= require('../utils/database');
 module.exports = class Cart{
 
     constructor(user_id, item_id, quantity){
-        // this.title = title;
-        // this.image = image;
-        // this.price = price;
+        
         this.user_id = user_id;
         this.item_id = item_id;
-        this.quantity = quantity; 
+       // this.quantity = quantity; 
     }
 
     // add_prod(){
     //     return pool.query('INSERT INTO products(user_id, item_id, quantity) VALUES ($1, $2, $3, $4);', [this.title, this.price, this.image, this.quantity]);
     // }
     add_to_cart(){
-        return pool.query('INSERT INTO cart(user_id, item_id, quantity) VALUES ($1, $2, $3);', [this.user_id, this.item_id, this.quantity]);
+        return pool.query('INSERT INTO cart(user_id, item_id, 1) VALUES ($1, $2);', [this.user_id, this.item_id]);
     }
     static get_all(){
         return pool.query('SELECT title, image, price, cart.quantity as qty FROM cart, products where cart.item_id = products.id;');
     }
     inc_quantity(){
-        return pool.query('INSERT INTO cart(user_id, item_id, quantity) VALUES ($1, $2, $3);', [this.user_id, this.item_id, 1]);
+        return pool.query('UPDATE cart set quantity = quantity + 1 where item_id = $2 and user_id = $1;', [this.user_id, this.item_id]);
     }
     static get_cred(){
         return pool.query('SELECT credit FROM users where users.user_id = 1;');
