ee70dec7acad0d532dfc2a7a082f7ba44e4ef769
Devansh Chandak <51949082+dchandak99@users.noreply.github.com>
2021-03-16 10:12:35 +0530

    Update cart.js

--- LAB5/models/cart.js
+++ LAB5/models/cart.js
@@ -1,7 +1,7 @@
 const pool= require('../utils/database');
 module.exports = class Cart{
 
-    constructor(user_id, item_id){
+    constructor(user_id, item_id, quantity){
         
         this.user_id = user_id;
         this.item_id = item_id;
@@ -12,10 +12,25 @@ module.exports = class Cart{
     //     return pool.query('INSERT INTO products(user_id, item_id, quantity) VALUES ($1, $2, $3, $4);', [this.title, this.price, this.image, this.quantity]);
     // }
     add_to_cart(){
-        return pool.query('INSERT INTO cart(user_id, item_id, 1) VALUES ($1, $2);', [this.user_id, this.item_id]);
+        //return pool.query('INSERT INTO cart(user_id, item_id, 1) VALUES ($1, $2);', [this.user_id, this.item_id]);
+        
+        pool
+            .query('SELECT quantity FROM cart WHERE user_id = $1 and item_id = $2;', [this.user_id, this.item_id])
+            .then((value) => {
+                if(value.rows.length == 0){
+                    pool.query('INSERT INTO cart(user_id, item_id, quantity) VALUES ($1, $2, $3);', [this.user_id, this.item_id, 1]);
+                }else{
+                    pool.query('UPDATE cart SET quantity = $3 WHERE user_id = $1 and item_id = $2;', [this.user_id, this.item_id, value.rows[0]["quantity"]+1]);
+                }
+            })
+            .catch(err => console.log(err));
+        
+        return Promise.resolve(0);
+    
+    
     }
     static get_all(){
-        return pool.query('SELECT title, image, price, cart.quantity as qty FROM cart, products where cart.item_id = products.id;');
+        return pool.query('SELECT user_id, item_id, title, image, price, cart.quantity FROM cart, products where cart.item_id = products.id;');
     }
     inc_quantity(){
         return pool.query('UPDATE cart set quantity = quantity + 1 where item_id = $2 and user_id = $1;', [this.user_id, this.item_id]);
@@ -23,4 +38,9 @@ module.exports = class Cart{
     static get_cred(){
         return pool.query('SELECT credit FROM users where users.user_id = 1;');
     }
+
+    static delete_all(){
+        return pool.query('DELETE FROM cart;');
+    }
+
 };
